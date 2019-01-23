fe69970d8f924273191c1276e77602f9e03eb8be
Soren Rasmussen <s.c.rasmussen@gmail.com>
2019-01-23 02:41:09 -0800

    adding options to install

--- strategoxt/installStrategoxt.sh
+++ strategoxt/installStrategoxt.sh
@@ -7,10 +7,12 @@
 STRATEGOXTDIR=strategoxt-0.17
 STRATEGOXTTAR=${STRATEGOXTDIR}.tar.gz
 STRATEGOXTURL=http://ftp.strategoxt.org/pub/stratego/StrategoXT/strategoxt-0.17/${STRATEGOXTTAR}
-ATERMDIR=aterm-2.5
+ATERMDIR=aterm-3.0
 ATERMTAR=${ATERMDIR}.tar.gz
-ATERMURL=http://releases.strategoxt.org/strategoxt-0.17/aterm/aterm-2.5pre21238-26ra85lr/${ATERMTAR}
-ATERMURLALT=http://ftp.strategoxt.org/pub/stratego/sdf2/sdf2-bundle-2.3.3/sdf2-bundle-2.3.3.tar.gz
+ATERMURL=https://github.com/rasmussn/aterms/releases/download/v3.0/aterm-3.0.tar.gz
+# Unmodified Aterm files that require 32 bit -m32 install
+ATERMURLALT32_1=http://releases.strategoxt.org/strategoxt-0.17/aterm/aterm-2.5pre21238-26ra85lr/${ATERMTAR}
+ATERMURLALT32_2=http://ftp.strategoxt.org/pub/stratego/sdf2/sdf2-bundle-2.3.3/sdf2-bundle-2.3.3.tar.gz
 SDF2DIR=sdf2-bundle-2.4
 SDF2TAR=${SDF2DIR}.tar.gz
 SDF2URL=http://releases.strategoxt.org/strategoxt-0.17/sdf2-bundle/sdf2-bundle-2.4pre212034-37nm9z7p/${SDF2TAR}
@@ -32,9 +34,21 @@ for key in "$@"; do
 	    echo "--prefix=/install/dir"
             echo "-h, --help", $key
             ;;
+	--clean)
+	    clean=true
+	    ;;
+	-d|--download)
+	    download=true
+	    ;;
+	-i|--install)
+	    install=true
+	    ;;
         --prefix=*)
             PREFIX="${key#*=}"
             ;;
+	-u|--untar)
+	    untar=true
+	    ;;
         # TO DO
         # cflags=*)
         #    cflags = "-m 32" + "USER STRING"
@@ -43,78 +57,90 @@ for key in "$@"; do
     shift
 done
 
-# download files if they aren't in current directory
-if [ ! -f ${STRATEGOXTTAR} ] ; then
-    wget ${STRATEGOXTURL}
-fi
-if [ ! -f ${ATERMTAR} ] ; then
-    wget ${ATERMURL}
-fi
-if [ ! -f ${SDF2TAR} ] ; then
-    wget ${SDF2URL}
+if [ "$download" = true ] ; then
+    # download files if they aren't in current directory
+    if [ ! -f ${STRATEGOXTTAR} ] ; then
+	wget ${STRATEGOXTURL}
+    fi
+    if [ ! -f ${ATERMTAR} ] ; then
+	wget ${ATERMURL}
+    fi
+    if [ ! -f ${SDF2TAR} ] ; then
+	wget ${SDF2URL}
+    fi
 fi
 
 # untar files if they aren't there
-if [ ! -d ${STRATEGOXTDIR} ] ; then
-    cmd="tar zxf ${STRATEGOXTTAR}"
-    echo $cmd
-    eval $cmd
-fi
-if [ ! -d ${ATERMDIR} ] ; then
-    cmd="tar zxf ${ATERMTAR}"
-    echo $cmd
-    eval $cmd
-fi
-if [ ! -d ${SDF2DIR} ] ; then
-    cmd="tar zxf ${SDF2TAR}"
-    echo $cmd
-    eval $cmd
-fi
-
-# install aterm
-install_aterm=false
-if [ "$install_aterm" = true ] ; then
-    cd ${ATERMDIR}
-    cmd="./configure --prefix=${PREFIX}/aterm CFLAGS=$CFLAGS"
-    echo $cmd
-    eval $cmd
-    echo "make"
-    make
-    echo "make install"
-    make install
-    echo ; echo "---------------ATERM INSTALLED---------------" ; echo
-    cd ..
+if [ "$untar" = true ] ; then
+    if [ ! -d ${STRATEGOXTDIR} ] ; then
+	cmd="tar zxf ${STRATEGOXTTAR}"
+	echo $cmd
+	eval $cmd
+    fi
+    if [ ! -d ${ATERMDIR} ] ; then
+	cmd="tar zxf ${ATERMTAR}"
+	echo $cmd
+	eval $cmd
+    fi
+    if [ ! -d ${SDF2DIR} ] ; then
+	cmd="tar zxf ${SDF2TAR}"
+	echo $cmd
+	eval $cmd
+    fi
 fi
 
+# install files
+if [ "$install" = true ] ; then
+    # install aterm
+    install_aterm=false
+    if [ "$install_aterm" = true ] ; then
+	cd ${ATERMDIR}
+	cmd="./configure --prefix=${PREFIX}/aterm CFLAGS=$CFLAGS"
+	echo $cmd
+	eval $cmd
+	echo "make"
+	make
+	echo "make install"
+	make install
+	echo ; echo "---------------ATERM INSTALLED---------------" ; echo
+	cd ..
+    fi
 
-# install sdf2-bundle
-install_sdf2=false
-if [ "$install_sdf2" = true ] ; then
-    cd ${SDF2DIR}
-    cmd="./configure --prefix=${PREFIX}/sdf2-bundle --with-aterm=${PREFIX}/aterm CFLAGS=$CFLAGS"
-    echo $cmd
-    eval $cmd
-    echo "make"
-    make
-    echo "make install"
-    make install
-    echo ; echo "---------------SDF2 INSTALLED---------------" ; echo
-    cd ..
+    # install sdf2-bundle
+    install_sdf2=false
+    if [ "$install_sdf2" = true ] ; then
+	cd ${SDF2DIR}
+	cmd="./configure --prefix=${PREFIX}/sdf2-bundle --with-aterm=${PREFIX}/aterm CFLAGS=$CFLAGS"
+	echo $cmd
+	eval $cmd
+	echo "make"
+	make
+	echo "make install"
+	make install
+	echo ; echo "---------------SDF2 INSTALLED---------------" ; echo
+	cd ..
+    fi
+
+    # install strategoxt
+    install_stratego=false
+    if [ "$install_stratego" = true ] ; then
+	cd ${STRATEGOXTDIR}
+	cmd="./configure --prefix=${PREFIX}/strategoxt --with-aterm=${PREFIX}/aterm --with-sdf=${PREFIX}/sdf2-bundle CFLAGS=$CFLAGS"
+	echo $cmd
+	eval $cmd
+	echo "make"
+	make
+	echo "make install"
+	make install
+	echo ; echo "---------------STRATEGOXT INSTALLED---------------" ; echo
+	cd ..
+    fi
 fi
 
-# install strategoxt
-install_stratego=true
-if [ "$install_stratego" = true ] ; then
-    cd ${STRATEGOXTDIR}
-    cmd="./configure --prefix=${PREFIX}/strategoxt --with-aterm=${PREFIX}/aterm --with-sdf=${PREFIX}/sdf2-bundle CFLAGS=$CFLAGS"
+if [ "$clean" = true ] ; then
+    cmd="rm -rf ${ATERMDIR} ${SDF2DIR} ${STRATEGOXTDIR}"
     echo $cmd
     eval $cmd
-    echo "make"
-    make
-    echo "make install"
-    make install
-    echo ; echo "---------------STRATEGOXT INSTALLED---------------" ; echo
-    cd ..
 fi
 
 exit 1
